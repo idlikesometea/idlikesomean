48ed336d4c61d93f24507bdff2036071a8305d43
idlikesometea <im@virket.com>
2020-07-09 21:11:22 -0500

    optimize back end

---
+++ backend/controllers/user.js
@@ -0,0 +1,63 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+
+exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(result => {
+          res.status(201).json({
+            message: 'Success',
+            result: result
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
+            message: 'Invalid authentication credentials!'
+          });
+        });
+    });
+};
+
+exports.userLogin = (req, res, next) => {
+  let userData;
+  User.findOne({ email: req.body.email })
+  .then(user => {
+    if (!user) {
+      return res.status(401).json({
+        message: 'Auth failed'
+      });
+    }
+    userData = user;
+    return bcrypt.compare(req.body.password, user.password);
+  })
+  .then(result => {
+    if (!result) {
+      return res.status.json(401)({
+        message: 'Auth failed'
+      });
+    }
+    const token = jwt.sign(
+      { email: userData.email, userId:userData._id },
+      'secret_tmp',
+      { expiresIn: '1h' }
+    );
+    res.status(200).json({
+      token: token,
+      expiresIn: 3600,
+      userId: userData._id,
+      message: 'success'
+    });
+  })
+  .catch(err => {
+    return res.status(401).json({
+      message: 'Invalid authentication credentials'
+    });
+  });
+};
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,153 +1,20 @@
 const express = require("express");
-const multer = require("multer");
 
-const Post = require("../models/posts");
+const postsController = require("../controllers/post");
+
 const checkAuth = require("../middleware/check-auth");
+const extractFile = require("../middleware/file");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpeg',
-  'image/jpg': 'jpg'
-};
+router.post("", checkAuth, extractFile, postsController.createPost);
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error('Invalid mime Type');
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '.' + ext);
-  }
-});
+router.put("/:id", checkAuth, extractFile, postsController.updatePost);
 
-router.post(
-  "",
-  checkAuth,
-  multer({storage:storage}).single("image"),
-  (req, res, next) => {
-    const url = req.protocol + '://' + req.get("host");
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: url + "/images/" + req.file.filename,
-      creator: req.userData.userId
-    });
-    post.save().then(result => {
-      res.status(201).json({
-        message: "Success",
-        post: {
-          ...result,
-          id: result._id
-        }
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: 'Creating a post failed'
-      })
-    });
-});
+router.get("", postsController.getPosts);
 
-router.put(
-  "/:id",
-  checkAuth,
-  multer({storage:storage}).single("image"), (req, res, next) => {
-    let imagePath = req.body.imagePath;
-    if (req.file) {
-      const url = req.protocol + '://' + req.get("host");
-      imagePath = url + "/images/" + req.file.filename;
-    }
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: imagePath,
-      creator: req.userData.userId
-    })
-    Post.updateOne({_id: req.params.id, creator: req.userData.userId}, post).then(result => {
-      if (result.nModified) {
-        res.status(200).json({
-          message: 'Update successful'
-        });
-      } else {
-        res.status(401).json({
-          message: 'Not authorized'
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: 'Could not update post'
-      })
-    });
-});
+router.get("/:id", postsController.getPost)
 
-router.get("", (req, res, next) => {
-  const pageSize = +req.query.pagesize;
-  const currentPage = +req.query.page;
-  const postQuery = Post.find();
-  let fetchedPosts;
-  if (pageSize && currentPage) {
-    postQuery
-      .skip(pageSize * (currentPage - 1))
-      .limit(pageSize);
-  }
-  postQuery.then(docs => {
-    fetchedPosts = docs;
-    return Post.count();
-  })
-  .then(count => {
-    res.status(200).json({
-      message: "Success",
-      posts: fetchedPosts,
-      total: count
-    });
-  })
-  .catch(err => {
-    res.status(501).json({
-      message: "Fetching posts failed"
-    });
-  });
-});
-
-router.get("/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({
-        message: 'Post not found'
-      })
-    }
-  })
-  .catch(err => {
-    res.status(500).json({
-      message: "Fetching post failed"
-    });
-  });
-})
-
-router.delete("/:id", checkAuth, (req, res) => {
-  Post.deleteOne({_id: req.params.id, creator: req.userData.userId}).then(result => {
-    if (result.n) {
-      res.status(200).json({message:"Deleted"});
-    } else {
-      res.status(401).json({message:"Not authorized"});
-    }
-  })
-  .catch(err => {
-    res.status(501).json({
-      message: "Deleting post failed"
-    });
-  });;
-});
+router.delete("/:id", checkAuth, postsController.deletePost);
 
 module.exports = router;
